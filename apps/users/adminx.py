32c70981ef8f40108f17c96a9d55a417e7fa08b3
ackerlyc <zhifengcui12@gmail.com>
2024-10-27 19:05:38 -0700

    Initial commit

---
+++ apps/users/adminx.py
@@ -0,0 +1,29 @@
+# users/adminx.py
+ 
+
+import xadmin
+from xadmin import views
+from .models import VerifyCode
+
+
+class BaseSetting(object):
+    #添加主题功能
+    enable_themes = True
+    use_bootswatch = True
+
+
+class GlobalSettings(object):
+    #全局配置，后台管理标题和页脚
+    site_title = "Shoppix"
+    site_footer = "Shoppix 2024"
+    #菜单收缩
+    menu_style = "accordion"
+
+
+class VerifyCodeAdmin(object):
+    list_display = ['code', 'mobile', "add_time"]
+
+
+xadmin.site.register(VerifyCode, VerifyCodeAdmin)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSettings)
